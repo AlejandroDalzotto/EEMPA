51295ddf108cb8b38378be1665e2f0f735dd94f7
kjistik <coronelmartinernesto@gmail.com>
2023-11-28 17:18:29 -0300

    removed useless import

--- src/main/java/chinchulin/varano/Models/Subject.java
+++ src/main/java/chinchulin/varano/Models/Subject.java
@@ -3,7 +3,6 @@ package chinchulin.varano.Models;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
