c263c0240399e93a2ea90d3b67e096c29de7bc7c
kjistik <coronelmartinernesto@gmail.com>
2023-11-28 00:59:35 -0300

    first commit stuff

---
+++ src/test/java/chinchulin/varano/VaranoApplicationTests.java
@@ -0,0 +1,13 @@
+package chinchulin.varano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaranoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
