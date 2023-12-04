fb95b82aa096302a76e262232cf2cd2b049c8582
kjistik <coronelmartinernesto@gmail.com>
2023-12-04 19:36:38 -0300

    new entities

--- src/main/java/chinchulin/varano/Models/Calification.java
+++ src/main/java/chinchulin/varano/Models/Calification.java
@@ -1,8 +1,12 @@
 package chinchulin.varano.Models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,8 +21,11 @@ public class Calification {
     @Column(name = "id_calification")
     Long id_calification;
 
-    @Column(name ="value")
+    @Column(name = "value")
     String value;
 
-    
+    @JsonBackReference(value = "student_module")
+    @ManyToOne
+    @JoinColumn(name = "student-subject")
+    Student_subject student_subject;
 }
