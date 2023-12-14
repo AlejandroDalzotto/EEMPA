7f3041dfcfcdac5f34df04f42705297865348d2c
kjistik <coronelmartinernesto@gmail.com>
2023-12-13 22:53:50 -0300

    corregí nombres

--- src/main/java/chinchulin/varano/Repositories/CalificationRepo.java
+++ src/main/java/chinchulin/varano/Repositories/CalificationRepo.java
@@ -1,14 +1,15 @@
 package chinchulin.varano.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import chinchulin.varano.Models.Calification;
-import chinchulin.varano.Models.Student;
 
 public interface CalificationRepo extends JpaRepository<Calification, Long> {
 
-    @Query(value = "SELECT * FROM calification u WHERE u.student-subject = :student-subject", nativeQuery = true)
-    Student getByDNI(@Param("student-subject") Long student_subject);
+    @Query(value = "SELECT * FROM calification u WHERE u.student_subject = :student-subject", nativeQuery = true)
+    List<Calification> getByStudentAndSubject(@Param("student-subject") Long student_subject);
 }
